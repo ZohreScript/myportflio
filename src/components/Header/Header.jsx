2e8c0c72234a9482793f2d9b450f28effdb7e2a5
zohreScript <z.omidi74@gmail.com>
2024-01-11 23:51:15 +0330

    update

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -64,11 +64,11 @@ const Header = () => {
           </div>
           {/* menu right */}
           <div className="flex items-center gap-4">
-            <button className="flex items-center gap-2 text-smallTextColor font-[600] border border-solid 
+            <a href="mailto:z.omidi74@gmail.com" className="flex items-center gap-2 text-smallTextColor font-[600] border border-solid 
             border-smallTextColor py-2 px-0 sm:px-2  rounded-[8px] max-h-[40px] hover:bg-smallTextColor hover:text-white
              ease-in duration-300">
               <i class="ri-send-plane-line"></i>Let's talk
-            </button>
+            </a>
 
             <span onClick={toggleMenu} className="text-2xl text-smallTextColor md:hidden cursor-pointer">
               <i class="ri-menu-line"></i></span>
