4bc9872763f190a98040cb720ef2902c0f884c61
ZohreScript <z.omidi74@gmail.com>
2024-05-21 18:03:44 +0330

    fix  commit

--- src/assets/data/portfolioData.js
+++ src/assets/data/portfolioData.js
@@ -8,98 +8,97 @@ import portfolioImg07 from "../images/portfolio-07.jpg";
 import portfolioImg08 from "../images/portfolio-08.jpg";
 import portfolioImg09 from "../images/portfolio-09.jpg";
 
-const portfolios = [
-  {
-    id: "01",
-    imgUrl: portfolioImg01,
-    category: "Web Design",
-    title: "Agency Website",
-    description:
-      "Lorem ipsum dolor sit,saepe!",
-    technologies: ["React.js", "Tailwind css"],
-    siteUrl: "https://agency-website-orpin.vercel.app/",
-  },
-  {
-    id: "02",
-    imgUrl: portfolioImg04,
-    category: "Web Design",
-    title: "Note Pad",
-    description:
-      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium vitae sunt magnam numquam esse ipsam? Quis qui enim, temporibus dignissimos atque repellendus amet a velit. Dolorem quidem animi doloribus saepe!",
-    technologies: ["React.js", "Tailwind css"],
-    siteUrl: "https://notepad-ebon.vercel.app/",
-  },
-  {
-    id: "03",
-    imgUrl: portfolioImg02,
-    category: "Web Design",
-    title: "shopcart",
-    description:
-      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium vitae sunt magnam numquam esse ipsam? Quis qui enim, temporibus dignissimos atque repellendus amet a velit. Dolorem quidem animi doloribus saepe!",
-    technologies: ["React", "Tailwind css",],
-    siteUrl: "https://shop-cart-navy.vercel.app/",
-  },
-  {
-    id: "04",
-    imgUrl: portfolioImg03,
-    category: "Web Design",
-    title: "inventory",
-    description:
-      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium vitae sunt magnam numquam esse ipsam? Quis qui enim, temporibus dignissimos atque repellendus amet a velit. Dolorem quidem animi doloribus saepe!",
-    technologies: ["React.js", "Tailwind css", "local storage"],
-    siteUrl: "https://zohrescript.github.io/inventory/",
-  },
+const portfolios = [{
+        id: "01",
+        imgUrl: portfolioImg01,
+        category: "Web Design",
+        title: "Agency Website",
+        description: "Lorem ipsum dolor sit,saepe!",
+        technologies: ["React.js", "Tailwind css"],
+        siteUrl: "https://agency-website-orpin.vercel.app/",
+    },
+    {
+        id: "02",
+        imgUrl: portfolioImg04,
+        category: "Web Design",
+        title: "Admin Dashboard",
+        description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium vitae sunt magnam numquam esse ipsam? Quis qui enim, temporibus dignissimos atque repellendus amet a velit. Dolorem quidem animi doloribus saepe!",
+        technologies: ["React.js", "Tailwind css"],
+        siteUrl: "https://github.com/ZohreScript/AdminDahboard",
+    },
+    {
+        id: "03",
+        imgUrl: portfolioImg02,
+        category: "Web Design",
+        title: "shopcart",
+        description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium vitae sunt magnam numquam esse ipsam? Quis qui enim, temporibus dignissimos atque repellendus amet a velit. Dolorem quidem animi doloribus saepe!",
+        technologies: ["React", "Tailwind css", ],
+        siteUrl: "https://shop-cart-navy.vercel.app/",
+    },
+    {
+        id: "04",
+        imgUrl: portfolioImg03,
+        category: "Web Design",
+        title: "inventory",
+        description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium vitae sunt magnam numquam esse ipsam? Quis qui enim, temporibus dignissimos atque repellendus amet a velit. Dolorem quidem animi doloribus saepe!",
+        technologies: ["React.js", "Tailwind css", "local storage"],
+        siteUrl: "https://zohrescript.github.io/inventory/",
+    },
 
-  {
-    id: "05",
-    imgUrl: portfolioImg05,
-    category: "Ux",
-    title: "portfolio",
-    description:
-      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium vitae sunt magnam numquam esse ipsam? Quis qui enim, temporibus dignissimos atque repellendus amet a velit. Dolorem quidem animi doloribus saepe!",
-    technologies: ["html", " css", "js"],
-    siteUrl: "https://portfolio-black-ten-93.vercel.app/",
-  },
-  {
-    id: "06",
-    imgUrl: portfolioImg06,
-    category: "Web Design",
-    title: "movie episodes website",
-    description:
-      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium vitae sunt magnam numquam esse ipsam? Quis qui enim, temporibus dignissimos atque repellendus amet a velit. Dolorem quidem animi doloribus saepe!",
-    technologies: ["React.js", "Tailwind css"],
-    siteUrl: "https://github.com/ZohreScript/Rickmorthy",
-  },
-  {
-    id: "07",
-    imgUrl: portfolioImg07,
-    category: "Web Design",
-    title: " my portfolio ",
-    description:
-      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium vitae sunt magnam numquam esse ipsam? Quis qui enim, temporibus dignissimos atque repellendus amet a velit. Dolorem quidem animi doloribus saepe!",
-    technologies: ["React.js", "Tailwind css",],
-    siteUrl: "https://zohrescript.github.io/myportflio/",
-  },
-  {
-    id: "08",
-    imgUrl: portfolioImg08,
-    category: "Web Design",
-    title: " fast food ",
-    description:
-      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium vitae sunt magnam numquam esse ipsam? Quis qui enim, temporibus dignissimos atque repellendus amet a velit. Dolorem quidem animi doloribus saepe!",
-    technologies: ["React.js", "Tailwind css",],
-    siteUrl: "https://food-opal-ten.vercel.app/",
-  },
-  {
-    id: "09",
-    imgUrl: portfolioImg09,
-    category: "Web Design",
-    title: " wetherApp ",
-    description:
-      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium vitae sunt magnam numquam esse ipsam? Quis qui enim, temporibus dignissimos atque repellendus amet a velit. Dolorem quidem animi doloribus saepe!",
-    technologies: ["React.js", "Tailwind css",],
-    siteUrl: "https://weather-app-omega-steel.vercel.app/",
-  },
+    {
+        id: "05",
+        imgUrl: portfolioImg05,
+        category: "Ux",
+        title: "portfolio",
+        description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium vitae sunt magnam numquam esse ipsam? Quis qui enim, temporibus dignissimos atque repellendus amet a velit. Dolorem quidem animi doloribus saepe!",
+        technologies: ["html", " css", "js"],
+        siteUrl: "https://portfolio-black-ten-93.vercel.app/",
+    },
+    {
+        id: "06",
+        imgUrl: portfolioImg06,
+        category: "Web Design",
+        title: "movie episodes website",
+        description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium vitae sunt magnam numquam esse ipsam? Quis qui enim, temporibus dignissimos atque repellendus amet a velit. Dolorem quidem animi doloribus saepe!",
+        technologies: ["React.js", "Tailwind css"],
+        siteUrl: "https://episode-movie.vercel.app/",
+    },
+    {
+        id: "07",
+        imgUrl: portfolioImg07,
+        category: "Web Design",
+        title: " my portfolio ",
+        description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium vitae sunt magnam numquam esse ipsam? Quis qui enim, temporibus dignissimos atque repellendus amet a velit. Dolorem quidem animi doloribus saepe!",
+        technologies: ["React.js", "Tailwind css", ],
+        siteUrl: "https://zohrescript.github.io/myportflio/",
+    },
+    {
+        id: "08",
+        imgUrl: portfolioImg08,
+        category: "Web Design",
+        title: " fast food ",
+        description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium vitae sunt magnam numquam esse ipsam? Quis qui enim, temporibus dignissimos atque repellendus amet a velit. Dolorem quidem animi doloribus saepe!",
+        technologies: ["React.js", "Tailwind css", ],
+        siteUrl: "https://food-opal-ten.vercel.app/",
+    },
+    {
+        id: "09",
+        imgUrl: portfolioImg09,
+        category: "Web Design",
+        title: " wetherApp ",
+        description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium vitae sunt magnam numquam esse ipsam? Quis qui enim, temporibus dignissimos atque repellendus amet a velit. Dolorem quidem animi doloribus saepe!",
+        technologies: ["React.js", "Tailwind css", ],
+        siteUrl: "https://weather-app-omega-steel.vercel.app/",
+    },
+    {
+        id: "10",
+        imgUrl: portfolioImg04,
+        category: "Web Design",
+        title: "Note Pad",
+        description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium vitae sunt magnam numquam esse ipsam? Quis qui enim, temporibus dignissimos atque repellendus amet a velit. Dolorem quidem animi doloribus saepe!",
+        technologies: ["React.js", "Tailwind css"],
+        siteUrl: "https://notepad-ebon.vercel.app/",
+    },
 ];
 
 export default portfolios;
